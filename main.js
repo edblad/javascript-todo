5fb86f64664b8ff6cfa357585dde65a3894d1a19
edblad <helena.edblad@gmail.com>
2018-02-21 22:10:18 +0100

    trying to get localstoage to work

--- main.js
+++ main.js
@@ -2,17 +2,28 @@ const todoInput = document.getElementById('todoInput');
 const submitTodo = document.getElementById('submitTodo');
 const todoList = document.getElementById('todoList');
 const completedList = document.getElementById('completedList');
+const deleteAllButton = document.getElementById('deleteAll');
 
 
 // Event listener for the add todo button
 submitTodo.addEventListener('click', function(event){
     event.preventDefault();
     //createTodo(todoInput.value);
-    addTodo();
-    showTodo();
+    
+    // Only add todo if it doesn't already exist
+    if(!todos.includes(todoInput.value)){ 
+        addTodo();
+        showTodo();
+    };
 });
 
 const todos = [];
+
+//function Todo(todo){
+//    this.todo = todo;
+//    this.completed = false;
+//}
+
 
 for(i = 0; i < localStorage.length; i++){
     let todo = '';
@@ -20,7 +31,9 @@ for(i = 0; i < localStorage.length; i++){
     todos.push(todo);
     
     const todoItem = document.createElement('li');
+    const paragraph = document.createElement('p');    
     var todoText = document.createTextNode(todo);
+    paragraph.appendChild(todoText);
     const deleteButton = document.createElement('button');
     const completeButton = document.createElement('button');
     
@@ -34,34 +47,34 @@ for(i = 0; i < localStorage.length; i++){
     
     todoItem.classList.add('todo');
     todoItem.appendChild(completeButton);
-    todoItem.appendChild(todoText);
+    todoItem.appendChild(paragraph);
     todoItem.appendChild(deleteButton);
     todoList.appendChild(todoItem);
 };
 
 console.log(todos);
 
-
-//var html = localStorage.getItem('todo');
-//todoList.innerHTML = html;
-
+/////// FUNCTIONS ///////
 function addTodo(){
+    //const newTodo = new Todo(todoInput.value, todoInput.value)
     todos.push(todoInput.value);
+    
     for(i = 0; i < todos.length; i++){
         localStorage.setItem('todo' + i, todos[i]);
+        const todo = localStorage.getItem('todo' + i);
     };
+    console.log(todos);
 };
 
 function showTodo(){
     const todoItem = document.createElement('li');
-    let todoText = '';
     const deleteButton = document.createElement('button');
     const completeButton = document.createElement('button');
-    
-    for(let todo of todos){
-        todoText = document.createTextNode(todo);
-    };
-    
+    const paragraph = document.createElement('p');
+    const todoText = document.createTextNode(todoInput.value);
+
+    paragraph.appendChild(todoText);
+
     completeButton.classList.add('completeButton');
     completeButton.innerHTML = '&#10004;';
     completeButton.addEventListener('click', completeTodo);
@@ -72,61 +85,56 @@ function showTodo(){
 
     todoItem.classList.add('todo');
     todoItem.appendChild(completeButton);
-    todoItem.appendChild(todoText);
+    todoItem.appendChild(paragraph);
     todoItem.appendChild(deleteButton);
 
     todoList.appendChild(todoItem);
 
-    const deleteAllButton = document.getElementById('deleteAll');
     if(todoList.children.length != 0){
         deleteAllButton.classList.add('show');
         deleteAllButton.addEventListener('click', deleteAll);
     };
-    
+
     todoInput.value = '';
-    
-    for(i = 0; i < todos.length; i++){
-        console.log(todos[i]);
-    };
 };
 
-/////// FUNCTIONS ///////
+
 /*  Create a new todo
  *  This will run when the user clicks on the "submitTodo"/Lägg till */
-function createTodo(input){
-    if(input){
-        const todoItem = document.createElement('li');
-        const todoText = document.createTextNode(input);
-        const deleteButton = document.createElement('button');
-        const completeButton = document.createElement('button');
-        
-        todos.push(input);
-        localStorage.setItem('todos', todos);
-        
-        completeButton.classList.add('completeButton');
-        completeButton.innerHTML = '&#10004;';
-        completeButton.addEventListener('click', completeTodo);
-        
-        deleteButton.classList.add('deleteButton');
-        deleteButton.innerHTML = '&#10008;';
-        deleteButton.addEventListener('click', deleteTodo);
-
-        todoItem.classList.add('todo');
-        todoItem.appendChild(completeButton);
-        todoItem.appendChild(todoText);
-        todoItem.appendChild(deleteButton);
-
-        todoList.appendChild(todoItem);
-        
-        const deleteAllButton = document.getElementById('deleteAll');
-        if(todoList.children.length != 0){
-            deleteAllButton.classList.add('show');
-            deleteAllButton.addEventListener('click', deleteAll);
-        };
-        
-        todoInput.value = '';
-    };
-};
+//function createTodo(input){
+//    if(input){
+//        const todoItem = document.createElement('li');
+//        const todoText = document.createTextNode(input);
+//        const deleteButton = document.createElement('button');
+//        const completeButton = document.createElement('button');
+//        
+//        todos.push(input);
+//        localStorage.setItem('todos', todos);
+//        
+//        completeButton.classList.add('completeButton');
+//        completeButton.innerHTML = '&#10004;';
+//        completeButton.addEventListener('click', completeTodo);
+//        
+//        deleteButton.classList.add('deleteButton');
+//        deleteButton.innerHTML = '&#10008;';
+//        deleteButton.addEventListener('click', deleteTodo);
+//
+//        todoItem.classList.add('todo');
+//        todoItem.appendChild(completeButton);
+//        todoItem.appendChild(todoText);
+//        todoItem.appendChild(deleteButton);
+//
+//        todoList.appendChild(todoItem);
+//        
+//        const deleteAllButton = document.getElementById('deleteAll');
+//        if(todoList.children.length != 0){
+//            deleteAllButton.classList.add('show');
+//            deleteAllButton.addEventListener('click', deleteAll);
+//        };
+//        
+//        todoInput.value = '';
+//    };
+//};
 
 // This will run when the user clicks on the complete todo button
 function completeTodo(){
@@ -140,7 +148,7 @@ function completeTodo(){
         deleteAllCompletedButton.addEventListener('click', deleteAllCompleted);
     };
     
-    const deleteAllButton = document.getElementById('deleteAll');
+    // Hide "Delete all" button when the last todo is completed
     if(todoList.children.length == 0){
         deleteAllButton.classList.remove('show');
     };
@@ -148,13 +156,43 @@ function completeTodo(){
     for(i = 0; i < todos.length; i++){
         console.log(todos[i]);
     };
+    console.log(todos);
 };
 
-// Delete the clicked item from the ul
-function deleteTodo(){
-    this.parentElement.remove();
+//function deleteFromArray(){
+//    const todoName = String(this.previousSibling.innerText);
+//    
+//    var index = todos.indexOf(todoName);
+//    
+//    if(todos[index] == todoName){
+//        todos.splice(index, 1);
+//
+//    }; 
+//    console.log(todos);
+//    deleteTodo();
+//};
     
-    const deleteAllButton = document.getElementById('deleteAll');
+
+// Delete the clicked item from the ul, array and local storage
+function deleteTodo(){
+    this.parentElement.remove(); // Remove from ul
+    
+    const todoName = String(this.previousSibling.innerText);
+    var index = todos.indexOf(todoName);
+    
+    if(todos[index] == todoName){
+        todos.splice(index, 1);
+    };
+    
+    for (let i = 0, iC = localStorage.length; i < iC; ++i) { 
+        var storageKey = localStorage.key(i);
+        console.log( storageKey + ': ' + localStorage.getItem(storageKey) );
+        localStorage.removeItem(storageKey + ': ' + localStorage.getItem(storageKey));
+    }
+    
+    console.log(todos);
+
+    // Hide "Delete all" button when the last todo is deleted
     if(todoList.children.length == 0){
         deleteAllButton.classList.remove('show');
     };
@@ -163,6 +201,8 @@ function deleteTodo(){
 // Delete all todo items from the todo list
 function deleteAll(){
     todoList.innerHTML = '';
+    const todos = [];
+    localStorage.clear();
     this.classList.remove('show');
 };
 
